6269a849ace143dae7b592598a54cdedf5ec2203
moon <mooncakeeos@yahoo.co.jp>
2019-08-29 10:17:29 +0800

    [refactor] fix demo to consolidate genesis file

--- demo/clean.sh
+++ demo/clean.sh
@@ -1,13 +1,10 @@
 #! /bin/bash
 
 killall -9 basecoin tepleton
-TMROOT=./data/chain1/tepleton tepleton unsafe_reset_all
-TMROOT=./data/chain2/tepleton tepleton unsafe_reset_all
-
-rm -rf ./data/chain1/basecoin/merkleeyes.db
-rm -rf ./data/chain2/basecoin/merkleeyes.db
+TMROOT=./data/chain1 tepleton unsafe_reset_all
+TMROOT=./data/chain2 tepleton unsafe_reset_all
 
 rm ./*.log
 
-rm ./data/chain1/tepleton/*.bak
-rm ./data/chain2/tepleton/*.bak
+rm ./data/chain1/*.bak
+rm ./data/chain2/*.bak
