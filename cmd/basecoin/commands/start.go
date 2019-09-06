c977e432d45ce17298dfbafdfadc4861fa8de6f2
ricardohsk <ricardohsk@outlook.com>
2019-09-06 15:50:19 +0800

    [add] support all flags when start

--- cmd/basecoin/commands/start.go
+++ cmd/basecoin/commands/start.go
@@ -28,36 +28,36 @@ var StartCmd = &cobra.Command{
 	RunE:  startCmd,
 }
 
-//flags
-var (
-	addrFlag              string
-	eyesFlag              string
-	withoutTendermintFlag bool
-)
-
 // TODO: move to config file
 const EyesCacheSize = 10000
 
-func init() {
+//nolint
+const (
+	FlagAddress           = "address"
+	FlagEyes              = "eyes"
+	FlagWithoutTendermint = "without-tepleton"
+)
 
-	flags := []Flag2Register{
-		{&addrFlag, "address", "tcp://0.0.0.0:46658", "Listen address"},
-		{&eyesFlag, "eyes", "local", "MerkleEyes address, or 'local' for embedded"},
-		{&withoutTendermintFlag, "without-tepleton", false, "Run Tendermint in-process with the App"},
-	}
-	RegisterFlags(StartCmd, flags)
+func init() {
+	flags := StartCmd.Flags()
+	flags.String(FlagAddress, "tcp://0.0.0.0:46658", "Listen address")
+	flags.String(FlagEyes, "local", "MerkleEyes address, or 'local' for embedded")
+	flags.Bool(FlagWithoutTendermint, false, "Only run basecoin wrsp app, assume external tepleton process")
+	// add all standard 'tepleton node' flags
+	tcmd.AddNodeFlags(StartCmd)
 }
 
 func startCmd(cmd *cobra.Command, args []string) error {
 	rootDir := viper.GetString(cli.HomeFlag)
+	meyes := viper.GetString(FlagEyes)
 
 	// Connect to MerkleEyes
 	var eyesCli *eyes.Client
-	if eyesFlag == "local" {
+	if meyes == "local" {
 		eyesCli = eyes.NewLocalClient(path.Join(rootDir, "data", "merkleeyes.db"), EyesCacheSize)
 	} else {
 		var err error
-		eyesCli, err = eyes.NewClient(eyesFlag)
+		eyesCli, err = eyes.NewClient(meyes)
 		if err != nil {
 			return errors.Errorf("Error connecting to MerkleEyes: %v\n", err)
 		}
@@ -91,7 +91,7 @@ func startCmd(cmd *cobra.Command, args []string) error {
 	}
 
 	chainID := basecoinApp.GetState().GetChainID()
-	if withoutTendermintFlag {
+	if viper.GetBool(FlagWithoutTendermint) {
 		logger.Info("Starting Basecoin without Tendermint", "chain_id", chainID)
 		// run just the wrsp app/server
 		return startBasecoinWRSP(basecoinApp)
@@ -104,7 +104,8 @@ func startCmd(cmd *cobra.Command, args []string) error {
 
 func startBasecoinWRSP(basecoinApp *app.Basecoin) error {
 	// Start the WRSP listener
-	svr, err := server.NewServer(addrFlag, "socket", basecoinApp)
+	addr := viper.GetString(FlagAddress)
+	svr, err := server.NewServer(addr, "socket", basecoinApp)
 	if err != nil {
 		return errors.Errorf("Error creating listener: %v\n", err)
 	}
