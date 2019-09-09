710f490fb4d7ddad6046ef3fdef93d6ccbe761d6
ricardohsk <ricardohsk@outlook.com>
2019-09-09 11:46:58 +0800

    [refactor] SendTx function, add new error Code

--- errors/common.go
+++ errors/common.go
@@ -7,8 +7,13 @@ package errors
 import wrsp "github.com/tepleton/wrsp/types"
 
 const (
-	msgDecoding     = "Error decoding input"
-	msgUnauthorized = "Unauthorized"
+	msgDecoding        = "Error decoding input"
+	msgUnauthorized    = "Unauthorized"
+	msgInvalidAddress  = "Invalid Address"
+	msgInvalidCoins    = "Invalid Coins"
+	msgInvalidSequence = "Invalid Sequence"
+	msgNoInputs        = "No Input Coins"
+	msgNoOutputs       = "No Output Coins"
 )
 
 func DecodingError() TMError {
@@ -17,4 +22,24 @@ func DecodingError() TMError {
 
 func Unauthorized() TMError {
 	return New(msgUnauthorized, wrsp.CodeType_Unauthorized)
+}
+
+func InvalidAddress() TMError {
+	return New(msgInvalidAddress, wrsp.CodeType_BaseInvalidInput)
+}
+
+func InvalidCoins() TMError {
+	return New(msgInvalidCoins, wrsp.CodeType_BaseInvalidInput)
+}
+
+func InvalidSequence() TMError {
+	return New(msgInvalidSequence, wrsp.CodeType_BaseInvalidInput)
+}
+
+func NoInputs() TMError {
+	return New(msgNoInputs, wrsp.CodeType_BaseInvalidInput)
+}
+
+func NoOutputs() TMError {
+	return New(msgNoOutputs, wrsp.CodeType_BaseInvalidOutput)
 }
