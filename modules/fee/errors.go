d1a59916995613e8b0943cdd422144f7fd05d6bb
ricardohsk <ricardohsk@outlook.com>
2019-09-10 23:15:18 +0800

    [refactor] finish error package refactoring

---
+++ modules/fee/errors.go
@@ -0,0 +1,19 @@
+package fee
+
+import (
+	rawerr "errors"
+
+	wrsp "github.com/tepleton/wrsp/types"
+	"github.com/tepleton/basecoin/errors"
+)
+
+var (
+	errInsufficientFees = rawerr.New("Insufficient Fees")
+)
+
+func ErrInsufficientFees() errors.TMError {
+	return errors.WithCode(errInsufficientFees, wrsp.CodeType_BaseInvalidInput)
+}
+func IsInsufficientFeesErr(err error) bool {
+	return errors.IsSameError(errInsufficientFees, err)
+}
