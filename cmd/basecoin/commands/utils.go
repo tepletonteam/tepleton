00942cf295eeda6a7b2bea5335cc7ddae6696149
tepleton <tepleton@outlook.com>
2019-09-11 09:58:22 +0800

    [refactor] remove lots of unused operations

--- cmd/basecoin/commands/utils.go
+++ cmd/basecoin/commands/utils.go
@@ -1,19 +1,6 @@
 package commands
 
-import (
-	"encoding/hex"
-	"fmt"
-
-	"github.com/pkg/errors"
-
-	wrsp "github.com/tepleton/wrsp/types"
-	wire "github.com/tepleton/go-wire"
-
-	"github.com/tepleton/basecoin/types"
-
-	client "github.com/tepleton/tepleton/rpc/client"
-	tmtypes "github.com/tepleton/tepleton/types"
-)
+import "encoding/hex"
 
 // Returns true for non-empty hex-string prefixed with "0x"
 func isHex(s string) bool {
@@ -33,78 +20,4 @@ func StripHex(s string) string {
 		return s[2:]
 	}
 	return s
-}
-
-// Query - send an wrsp query
-func Query(tmAddr string, key []byte) (*wrsp.ResultQuery, error) {
-	httpClient := client.NewHTTP(tmAddr, "/websocket")
-	return queryWithClient(httpClient, key)
-}
-
-func queryWithClient(httpClient *client.HTTP, key []byte) (*wrsp.ResultQuery, error) {
-	res, err := httpClient.WRSPQuery("/key", key, true)
-	if err != nil {
-		return nil, errors.Errorf("Error calling /wrsp_query: %v", err)
-	}
-	if !res.Code.IsOK() {
-		return nil, errors.Errorf("Query got non-zero exit code: %v. %s", res.Code, res.Log)
-	}
-	return res.ResultQuery, nil
-}
-
-// fetch the account by querying the app
-func getAccWithClient(httpClient *client.HTTP, address []byte) (*types.Account, error) {
-
-	key := types.AccountKey(address)
-	response, err := queryWithClient(httpClient, key)
-	if err != nil {
-		return nil, err
-	}
-
-	accountBytes := response.Value
-
-	if len(accountBytes) == 0 {
-		return nil, fmt.Errorf("Account bytes are empty for address: %X ", address) //never stack trace
-	}
-
-	var acc *types.Account
-	err = wire.ReadBinaryBytes(accountBytes, &acc)
-	if err != nil {
-		return nil, errors.Errorf("Error reading account %X error: %v",
-			accountBytes, err.Error())
-	}
-
-	return acc, nil
-}
-
-func getHeaderAndCommit(tmAddr string, height int) (*tmtypes.Header, *tmtypes.Commit, error) {
-	httpClient := client.NewHTTP(tmAddr, "/websocket")
-	res, err := httpClient.Commit(height)
-	if err != nil {
-		return nil, nil, errors.Errorf("Error on commit: %v", err)
-	}
-	header := res.Header
-	commit := res.Commit
-
-	return header, commit, nil
-}
-
-func waitForBlock(httpClient *client.HTTP) error {
-	res, err := httpClient.Status()
-	if err != nil {
-		return err
-	}
-
-	lastHeight := res.LatestBlockHeight
-	for {
-		res, err := httpClient.Status()
-		if err != nil {
-			return err
-		}
-		if res.LatestBlockHeight > lastHeight {
-			break
-		}
-
-	}
-	return nil
 }
