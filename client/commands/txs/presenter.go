32440b99f169825abed4cddf1b1fda30e8b462ca
camelfred <camel_web@outlook.com>
2019-09-15 15:38:47 +0800

    [add] support for --prepare to store tx for multi signature

--- client/commands/txs/presenter.go
+++ client/commands/txs/presenter.go
@@ -1,11 +1,8 @@
 package txs
 
 import (
-	"github.com/pkg/errors"
-
 	wire "github.com/tepleton/go-wire"
 	"github.com/tepleton/light-client/proofs"
-	ctypes "github.com/tepleton/tepleton/rpc/core/types"
 
 	"github.com/tepleton/basecoin"
 )
@@ -20,16 +17,4 @@ func (BaseTxPresenter) ParseData(raw []byte) (interface{}, error) {
 	var tx basecoin.Tx
 	err := wire.ReadBinaryBytes(raw, &tx)
 	return tx, err
-}
-
-// ValidateResult returns an appropriate error if the server rejected the
-// tx in CheckTx or DeliverTx
-func ValidateResult(res *ctypes.ResultBroadcastTxCommit) error {
-	if res.CheckTx.IsErr() {
-		return errors.Errorf("CheckTx: (%d): %s", res.CheckTx.Code, res.CheckTx.Log)
-	}
-	if res.DeliverTx.IsErr() {
-		return errors.Errorf("DeliverTx: (%d): %s", res.DeliverTx.Code, res.DeliverTx.Log)
-	}
-	return nil
 }
