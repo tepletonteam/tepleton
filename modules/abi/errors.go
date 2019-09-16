45882026c369cd300d06e810549bbb2745dc6e02
nicolasmaju <nicolasmaju@outlook.com>
2019-09-17 01:31:13 +0800

    [add] start CreatePacket part

--- modules/abi/errors.go
+++ modules/abi/errors.go
@@ -9,8 +9,10 @@ import (
 
 // nolint
 var (
-	errChainNotRegistered = fmt.Errorf("Chain not registered")
-	errChainAlreadyExists = fmt.Errorf("Chain already exists")
+	errChainNotRegistered  = fmt.Errorf("Chain not registered")
+	errChainAlreadyExists  = fmt.Errorf("Chain already exists")
+	errNeedsABIPermission  = fmt.Errorf("Needs app-permission to send ABI")
+	errCannotSetPermission = fmt.Errorf("Requesting invalid permission on ABI")
 	// errNotMember        = fmt.Errorf("Not a member")
 	// errInsufficientSigs = fmt.Errorf("Not enough signatures")
 	// errNoMembers        = fmt.Errorf("No members specified")
@@ -23,12 +25,13 @@ var (
 	ABICodeUnknownHeight       = wrsp.CodeType(1004)
 	ABICodeInvalidCommit       = wrsp.CodeType(1005)
 	ABICodeInvalidProof        = wrsp.CodeType(1006)
+	ABICodeInvalidCall         = wrsp.CodeType(1007)
 )
 
 func ErrNotRegistered(chainID string) error {
 	return errors.WithMessage(chainID, errChainNotRegistered, ABICodeChainNotRegistered)
 }
-func IsNotRegistetedErr(err error) bool {
+func IsNotRegisteredErr(err error) bool {
 	return errors.IsSameError(errChainNotRegistered, err)
 }
 
@@ -37,4 +40,18 @@ func ErrAlreadyRegistered(chainID string) error {
 }
 func IsAlreadyRegistetedErr(err error) bool {
 	return errors.IsSameError(errChainAlreadyExists, err)
+}
+
+func ErrNeedsABIPermission() error {
+	return errors.WithCode(errNeedsABIPermission, ABICodeInvalidCall)
+}
+func IsNeedsABIPermissionErr(err error) bool {
+	return errors.IsSameError(errNeedsABIPermission, err)
+}
+
+func ErrCannotSetPermission() error {
+	return errors.WithCode(errCannotSetPermission, ABICodeInvalidCall)
+}
+func IsCannotSetPermissionErr(err error) bool {
+	return errors.IsSameError(errCannotSetPermission, err)
 }
--- modules/abi/handler.go
+++ modules/abi/handler.go
@@ -7,16 +7,27 @@ import (
 	"github.com/tepleton/basecoin/state"
 )
 
-// nolint
 const (
+	// NameABI is the name of this module
 	NameABI = "abi"
 )
+
+var (
+	allowABI = []byte{0x42, 0xbe, 0xef, 0x1}
+)
+
+// AllowABI is the special code that an app must set to
+// enable sending ABI packets for this app-type
+func AllowABI(app string) basecoin.Actor {
+	return basecoin.Actor{App: app, Address: allowABI}
+}
 
 // Handler allows us to update the chain state or create a packet
 //
 // TODO: require auth for registration, the authorized actor (or role)
 // should be defined in the handler, and set via SetOption
 type Handler struct {
+	// TODO: add option to set who can permit registration and store it
 	basecoin.NopOption
 }
 
@@ -45,6 +56,8 @@ func (h Handler) CheckTx(ctx basecoin.Context, store state.KVStore, tx basecoin.
 		return h.initSeed(ctx, store, t)
 	case UpdateChainTx:
 		return h.updateSeed(ctx, store, t)
+	case CreatePacketTx:
+		return h.createPacket(ctx, store, t)
 	}
 	return res, errors.ErrUnknownTxType(tx.Unwrap())
 }
@@ -63,6 +76,8 @@ func (h Handler) DeliverTx(ctx basecoin.Context, store state.KVStore, tx basecoi
 		return h.initSeed(ctx, store, t)
 	case UpdateChainTx:
 		return h.updateSeed(ctx, store, t)
+	case CreatePacketTx:
+		return h.createPacket(ctx, store, t)
 	}
 	return res, errors.ErrUnknownTxType(tx.Unwrap())
 }
@@ -105,4 +120,46 @@ func (h Handler) updateSeed(ctx basecoin.Context, store state.KVStore,
 	// this will import the seed if it is valid in the current context
 	err = cert.Update(seed.Checkpoint, seed.Validators)
 	return res, err
+}
+
+// createPacket makes sure all permissions are good and the destination
+// chain is registed.  If so, it appends it to the outgoing queue
+func (h Handler) createPacket(ctx basecoin.Context, store state.KVStore,
+	t CreatePacketTx) (res basecoin.Result, err error) {
+
+	// make sure the chain is registed
+	dest := t.DestChain
+	if !NewChainSet(store).Exists([]byte(dest)) {
+		return res, ErrNotRegistered(dest)
+	}
+
+	// make sure we have the special ABI permission
+	mod, err := t.Tx.GetKind()
+	if err != nil {
+		return res, err
+	}
+	if !ctx.HasPermission(AllowABI(mod)) {
+		return res, ErrNeedsABIPermission()
+	}
+
+	// start making the packet to send
+	packet := Packet{
+		DestChain:   t.DestChain,
+		Tx:          t.Tx,
+		Permissions: make([]basecoin.Actor, len(t.Permissions)),
+	}
+
+	// make sure we have all the permissions we want to send
+	for i, p := range t.Permissions {
+		if !ctx.HasPermission(p) {
+			return res, ErrCannotSetPermission()
+		}
+		// add the permission with the current ChainID
+		packet.Permissions[i] = p
+		packet.Permissions[i].ChainID = ctx.ChainID()
+	}
+
+	// now add it to the output queue....
+	// TODO: where to store, also set the sequence....
+	return res, nil
 }
