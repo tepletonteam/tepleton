6be5ae40596f879e98a2f821bb1b4680c2824dd1
nicolasmaju <nicolasmaju@outlook.com>
2019-09-17 11:28:17 +0800

    [refactor] cleanup PostPacket handling

--- modules/abi/errors.go
+++ modules/abi/errors.go
@@ -11,21 +11,22 @@ import (
 var (
 	errChainNotRegistered  = fmt.Errorf("Chain not registered")
 	errChainAlreadyExists  = fmt.Errorf("Chain already exists")
+	errWrongDestChain      = fmt.Errorf("This is not the destination")
 	errNeedsABIPermission  = fmt.Errorf("Needs app-permission to send ABI")
 	errCannotSetPermission = fmt.Errorf("Requesting invalid permission on ABI")
-	// errNotMember        = fmt.Errorf("Not a member")
-	// errInsufficientSigs = fmt.Errorf("Not enough signatures")
-	// errNoMembers        = fmt.Errorf("No members specified")
-	// errTooManyMembers   = fmt.Errorf("Too many members specified")
-	// errNotEnoughMembers = fmt.Errorf("Not enough members specified")
+	errHeaderNotFound      = fmt.Errorf("Header not found")
+	errPacketAlreadyExists = fmt.Errorf("Packet already handled")
+	errPacketOutOfOrder    = fmt.Errorf("Packet out of order")
+	errInvalidProof        = fmt.Errorf("Invalid merkle proof")
 
-	ABICodeChainNotRegistered  = wrsp.CodeType(1001)
-	ABICodeChainAlreadyExists  = wrsp.CodeType(1002)
-	ABICodePacketAlreadyExists = wrsp.CodeType(1003)
-	ABICodeUnknownHeight       = wrsp.CodeType(1004)
-	ABICodeInvalidCommit       = wrsp.CodeType(1005)
-	ABICodeInvalidProof        = wrsp.CodeType(1006)
-	ABICodeInvalidCall         = wrsp.CodeType(1007)
+	ABICodeChainNotRegistered    = wrsp.CodeType(1001)
+	ABICodeChainAlreadyExists    = wrsp.CodeType(1002)
+	ABICodeUnknownChain          = wrsp.CodeType(1003)
+	ABICodeInvalidPacketSequence = wrsp.CodeType(1004)
+	ABICodeUnknownHeight         = wrsp.CodeType(1005)
+	ABICodeInvalidCommit         = wrsp.CodeType(1006)
+	ABICodeInvalidProof          = wrsp.CodeType(1007)
+	ABICodeInvalidCall           = wrsp.CodeType(1008)
 )
 
 func ErrNotRegistered(chainID string) error {
@@ -42,6 +43,13 @@ func IsAlreadyRegistetedErr(err error) bool {
 	return errors.IsSameError(errChainAlreadyExists, err)
 }
 
+func ErrWrongDestChain(chainID string) error {
+	return errors.WithMessage(chainID, errWrongDestChain, ABICodeUnknownChain)
+}
+func IsWrongDestChainErr(err error) bool {
+	return errors.IsSameError(errWrongDestChain, err)
+}
+
 func ErrNeedsABIPermission() error {
 	return errors.WithCode(errNeedsABIPermission, ABICodeInvalidCall)
 }
@@ -54,4 +62,34 @@ func ErrCannotSetPermission() error {
 }
 func IsCannotSetPermissionErr(err error) bool {
 	return errors.IsSameError(errCannotSetPermission, err)
+}
+
+func ErrHeaderNotFound(h int) error {
+	msg := fmt.Sprintf("height %d", h)
+	return errors.WithMessage(msg, errHeaderNotFound, ABICodeUnknownHeight)
+}
+func IsHeaderNotFoundErr(err error) bool {
+	return errors.IsSameError(errHeaderNotFound, err)
+}
+
+func ErrPacketAlreadyExists() error {
+	return errors.WithCode(errPacketAlreadyExists, ABICodeInvalidPacketSequence)
+}
+func IsPacketAlreadyExistsErr(err error) bool {
+	return errors.IsSameError(errPacketAlreadyExists, err)
+}
+
+func ErrPacketOutOfOrder(seq uint64) error {
+	msg := fmt.Sprintf("expected %d", seq)
+	return errors.WithMessage(msg, errPacketOutOfOrder, ABICodeInvalidPacketSequence)
+}
+func IsPacketOutOfOrderErr(err error) bool {
+	return errors.IsSameError(errPacketOutOfOrder, err)
+}
+
+func ErrInvalidProof() error {
+	return errors.WithCode(errInvalidProof, ABICodeInvalidProof)
+}
+func IsInvalidProofErr(err error) bool {
+	return errors.IsSameError(errInvalidProof, err)
 }
