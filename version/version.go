3c1cbbfa8529bad22849a1752e44c6827a55b4a3
ricardohsk <ricardohsk@outlook.com>
2019-09-22 16:55:27 -0800

    [refactor] update go version

--- version/version.go
+++ version/version.go
@@ -1,8 +1,12 @@
 //nolint
 package version
 
+// when updating these,
+// remember to also update examples/basecoin/tests/cli/rpc.sh
+// ... ugh ....
+
 const Maj = "0"
 const Min = "7"
-const Fix = "0"
+const Fix = "1"
 
-const Version = "0.7.0"
+const Version = "0.7.1"
