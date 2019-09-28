5fb3771a36d7a1633d6095dd14b274afa6ae6609
moon <mooncakeeos@yahoo.co.jp>
2019-09-27 21:47:53 -0800

    [add] make Context to be immutable

--- app/app.go
+++ app/app.go
@@ -8,10 +8,9 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/pkg/errors"
 	wrsp "github.com/tepleton/wrsp/types"
-	cmn "github.com/tepleton/tmlibs/common"
 	"github.com/tepleton/tmlibs/log"
 
-	sdk "github.com/tepleton/tepleton-sdk"
+	"github.com/tepleton/tepleton-sdk/types"
 )
 
 const mainKeyHeader = "header"
@@ -24,16 +23,16 @@ type App struct {
 	name string
 
 	// DeliverTx (main) state
-	store MultiStore
+	store types.MultiStore
 
 	// CheckTx state
-	storeCheck CacheMultiStore
+	storeCheck types.CacheMultiStore
 
 	// Current block header
 	header *wrsp.Header
 
 	// Handler for CheckTx and DeliverTx.
-	handler sdk.Handler
+	handler types.Handler
 
 	// Cached validator changes from DeliverTx
 	valUpdates []wrsp.Validator
@@ -48,17 +47,17 @@ func NewApp(name string) *App {
 	}
 }
 
-func (app *App) SetStore(store MultiStore) {
+func (app *App) SetStore(store types.MultiStore) {
 	app.store = store
 }
 
-func (app *App) SetHandler(handler Handler) {
+func (app *App) SetHandler(handler types.Handler) {
 	app.handler = handler
 }
 
 func (app *App) LoadLatestVersion() error {
 	store := app.store
-	store.LoadLastVersion()
+	store.LoadLatestVersion()
 	return app.initFromStore()
 }
 
@@ -108,12 +107,67 @@ func (app *App) initFromStore() error {
 
 //----------------------------------------
 
-// DeliverTx - WRSP - dispatches to the handler
+// Implements WRSP
+func (app *App) Info(req wrsp.RequestInfo) wrsp.ResponseInfo {
+
+	lastCommitID := app.store.LastCommitID()
+
+	return wrsp.ResponseInfo{
+		Data:             app.Name,
+		LastBlockHeight:  lastCommitID.Version,
+		LastBlockAppHash: lastCommitID.Hash,
+	}
+}
+
+// Implements WRSP
+func (app *App) SetOption(req wrsp.RequestSetOption) wrsp.ResponseSetOption {
+	return "Not Implemented"
+}
+
+// Implements WRSP
+func (app *App) InitChain(req wrsp.RequestInitChain) wrsp.ResponseInitChain {
+	// TODO: Use req.Validators
+}
+
+// Implements WRSP
+func (app *App) Query(req wrsp.RequestQuery) wrsp.ResponseQuery {
+	// TODO: See app/query.go
+}
+
+// Implements WRSP
+func (app *App) BeginBlock(req wrsp.RequestBeginBlock) wrsp.ResponseBeginBlock {
+	app.header = req.Header
+}
+
+// Implements WRSP
+func (app *App) CheckTx(txBytes []byte) wrsp.ResponseCheckTx {
+
+	// Initialize arguments to Handler.
+	var isCheckTx = true
+	var ctx = types.NewContext(app.header, isCheckTx, txBytes)
+	var store = app.store
+	var tx Tx = nil // nil until a decorator parses one.
+
+	// Run the handler.
+	var result = app.handler(ctx, app.store, tx)
+
+	// Tell the blockchain engine (i.e. Tendermint).
+	return wrsp.ResponseDeliverTx{
+		Code:      result.Code,
+		Data:      result.Data,
+		Log:       result.Log,
+		Gas:       result.Gas,
+		FeeDenom:  result.FeeDenom,
+		FeeAmount: result.FeeAmount,
+	}
+}
+
+// Implements WRSP
 func (app *App) DeliverTx(txBytes []byte) wrsp.ResponseDeliverTx {
 
 	// Initialize arguments to Handler.
 	var isCheckTx = false
-	var ctx = sdk.NewContext(app.header, isCheckTx, txBytes)
+	var ctx = types.NewContext(app.header, isCheckTx, txBytes)
 	var store = app.store
 	var tx Tx = nil // nil until a decorator parses one.
 
@@ -137,104 +191,16 @@ func (app *App) DeliverTx(txBytes []byte) wrsp.ResponseDeliverTx {
 	}
 }
 
-// CheckTx - WRSP - dispatches to the handler
-func (app *App) CheckTx(txBytes []byte) wrsp.ResponseCheckTx {
-
-	// Initialize arguments to Handler.
-	var isCheckTx = true
-	var ctx = sdk.NewContext(app.header, isCheckTx, txBytes)
-	var store = app.store
-	var tx Tx = nil // nil until a decorator parses one.
-
-	// Run the handler.
-	var result = app.handler(ctx, app.store, tx)
-
-	// Tell the blockchain engine (i.e. Tendermint).
-	return wrsp.ResponseDeliverTx{
-		Code:      result.Code,
-		Data:      result.Data,
-		Log:       result.Log,
-		Gas:       result.Gas,
-		FeeDenom:  result.FeeDenom,
-		FeeAmount: result.FeeAmount,
-	}
+// Implements WRSP
+func (app *App) EndBlock(req wrsp.RequestEndBlock) (res wrsp.ResponseEndBlock) {
+	// XXX Update to res.Updates.
+	res.ValidatorUpdates = app.valUpdates
+	app.valUpdates = nil
+	return
 }
 
-// Info - WRSP
-func (app *App) Info(req wrsp.RequestInfo) wrsp.ResponseInfo {
-
-	lastCommitID := app.store.LastCommitID()
-
-	return wrsp.ResponseInfo{
-		Data:             app.Name,
-		LastBlockHeight:  lastCommitID.Version,
-		LastBlockAppHash: lastCommitID.Hash,
-	}
-}
-
-// SetOption - WRSP
-func (app *App) SetOption(key string, value string) string {
-	return "Not Implemented"
-}
-
-// Query - WRSP
-func (app *App) Query(reqQuery wrsp.RequestQuery) (resQuery wrsp.ResponseQuery) {
-	/*
-		XXX Make this work with MultiStore.
-		XXX It will require some interfaces updates in store/types.go.
-
-		if len(reqQuery.Data) == 0 {
-			resQuery.Log = "Query cannot be zero length"
-			resQuery.Code = wrsp.CodeType_EncodingError
-			return
-		}
-
-		// set the query response height to current
-		tree := app.state.Committed()
-
-		height := reqQuery.Height
-		if height == 0 {
-			// TODO: once the rpc actually passes in non-zero
-			// heights we can use to query right after a tx
-			// we must retrun most recent, even if apphash
-			// is not yet in the blockchain
-
-			withProof := app.CommittedHeight() - 1
-			if tree.Tree.VersionExists(withProof) {
-				height = withProof
-			} else {
-				height = app.CommittedHeight()
-			}
-		}
-		resQuery.Height = height
-
-		switch reqQuery.Path {
-		case "/store", "/key": // Get by key
-			key := reqQuery.Data // Data holds the key bytes
-			resQuery.Key = key
-			if reqQuery.Prove {
-				value, proof, err := tree.GetVersionedWithProof(key, height)
-				if err != nil {
-					resQuery.Log = err.Error()
-					break
-				}
-				resQuery.Value = value
-				resQuery.Proof = proof.Bytes()
-			} else {
-				value := tree.Get(key)
-				resQuery.Value = value
-			}
-
-		default:
-			resQuery.Code = wrsp.CodeType_UnknownRequest
-			resQuery.Log = cmn.Fmt("Unexpected Query path: %v", reqQuery.Path)
-		}
-		return
-	*/
-}
-
-// Commit implements wrsp.Application
-func (app *App) Commit() (res wrsp.Result) {
+// Implements WRSP
+func (app *App) Commit() (res wrsp.ResponseCommit) {
 	commitID := app.store.Commit()
 	app.logger.Debug("Commit synced",
 		"commit", commitID,
@@ -242,22 +208,8 @@ func (app *App) Commit() (res wrsp.Result) {
 	return wrsp.NewResultOK(hash, "")
 }
 
-// InitChain - WRSP
-func (app *App) InitChain(req wrsp.RequestInitChain) {}
-
-// BeginBlock - WRSP
-func (app *App) BeginBlock(req wrsp.RequestBeginBlock) {
-	app.header = req.Header
-}
-
-// EndBlock - WRSP
-// Returns a list of all validator changes made in this block
-func (app *App) EndBlock(height uint64) (res wrsp.ResponseEndBlock) {
-	// XXX Update to res.Updates.
-	res.Diffs = app.valUpdates
-	app.valUpdates = nil
-	return
-}
+//----------------------------------------
+// Misc.
 
 // Return index of list with validator of same PubKey, or -1 if no match
 func pubKeyIndex(val *wrsp.Validator, list []*wrsp.Validator) int {
@@ -274,39 +226,4 @@ func pubKeyIndex(val *wrsp.Validator, list []*wrsp.Validator) int {
 // ResponseDeliverTx.Log and ResponseCheckTx.Log.
 func makeDefaultLogger() log.Logger {
 	return log.NewTMLogger(log.NewSyncWriter(os.Stdout)).With("module", "sdk/app")
-}
-
-// InitState - used to setup state (was SetOption)
-// to be call from setting up the genesis file
-func (app *InitApp) InitState(module, key, value string) error {
-	state := app.Append()
-	logger := app.Logger().With("module", module, "key", key)
-
-	if module == sdk.ModuleNameBase {
-		if key == sdk.ChainKey {
-			app.info.SetChainID(state, value)
-			return nil
-		}
-		logger.Error("Invalid genesis option")
-		return fmt.Errorf("Unknown base option: %s", key)
-	}
-
-	log, err := app.initState.InitState(logger, state, module, key, value)
-	if err != nil {
-		logger.Error("Invalid genesis option", "err", err)
-	} else {
-		logger.Info(log)
-	}
-	return err
-}
-
-// InitChain - WRSP - sets the initial validators
-func (app *InitApp) InitChain(req wrsp.RequestInitChain) {
-	// return early if no InitValidator registered
-	if app.initVals == nil {
-		return
-	}
-
-	logger, store := app.Logger(), app.Append()
-	app.initVals.InitValidators(logger, store, req.Validators)
 }
