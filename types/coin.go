eb62482f7017fd1051ddb88acb840cec52cd57a7
camelfred <camel_web@outlook.com>
2019-09-28 05:48:26 -0800

    [add] move coin to types

--- types/coin.go
+++ types/coin.go
@@ -2,49 +2,37 @@ package types
 
 import (
 	"fmt"
-	"regexp"
 	"sort"
-	"strconv"
 	"strings"
-
-	"github.com/pkg/errors"
 )
 
+// Coin hold some amount of one currency
 type Coin struct {
 	Denom  string `json:"denom"`
 	Amount int64  `json:"amount"`
 }
 
+// String provides a human-readable representation of a coin
 func (coin Coin) String() string {
 	return fmt.Sprintf("%v%v", coin.Amount, coin.Denom)
 }
 
-//regex codes for extracting coins from string
-var reDenom = regexp.MustCompile("")
-var reAmt = regexp.MustCompile("(\\d+)")
+// IsZero returns if this represents no money
+func (coin Coin) IsZero() bool {
+	return coin.Amount == 0
+}
 
-var reCoin = regexp.MustCompile("^([[:digit:]]+)[[:space:]]*([[:alpha:]]+)$")
-
-func ParseCoin(str string) (Coin, error) {
-	var coin Coin
-
-	matches := reCoin.FindStringSubmatch(strings.TrimSpace(str))
-	if matches == nil {
-		return coin, errors.Errorf("%s is invalid coin definition", str)
-	}
-
-	// parse the amount (should always parse properly)
-	amt, err := strconv.Atoi(matches[1])
-	if err != nil {
-		return coin, err
-	}
-
-	coin = Coin{matches[2], int64(amt)}
-	return coin, nil
+// IsGTE returns true if they are the same type and the receiver is
+// an equal or greater value
+func (coin Coin) IsGTE(other Coin) bool {
+	return (coin.Denom == other.Denom) &&
+		(coin.Amount >= other.Amount)
 }
 
 //----------------------------------------
+// Coins
 
+// Coins is a set of Coin, one per currency
 type Coins []Coin
 
 func (coins Coins) String() string {
@@ -59,33 +47,7 @@ func (coins Coins) String() string {
 	return out[:len(out)-1]
 }
 
-func ParseCoins(str string) (Coins, error) {
-	// empty string is empty list...
-	if len(str) == 0 {
-		return nil, nil
-	}
-
-	split := strings.Split(str, ",")
-	var coins Coins
-
-	for _, el := range split {
-		coin, err := ParseCoin(el)
-		if err != nil {
-			return coins, err
-		}
-		coins = append(coins, coin)
-	}
-
-	// ensure they are in proper order, to avoid random failures later
-	coins.Sort()
-	if !coins.IsValid() {
-		return nil, errors.Errorf("ParseCoins invalid: %#v", coins)
-	}
-
-	return coins, nil
-}
-
-// Must be sorted, and not have 0 amounts
+// IsValid asserts the Coins are sorted, and don't have 0 amounts
 func (coins Coins) IsValid() bool {
 	switch len(coins) {
 	case 0:
@@ -108,27 +70,28 @@ func (coins Coins) IsValid() bool {
 	}
 }
 
+// Plus combines to sets of coins
+//
 // TODO: handle empty coins!
 // Currently appends an empty coin ...
-func (coinsA Coins) Plus(coinsB Coins) Coins {
+func (coins Coins) Plus(coinsB Coins) Coins {
 	sum := []Coin{}
 	indexA, indexB := 0, 0
-	lenA, lenB := len(coinsA), len(coinsB)
+	lenA, lenB := len(coins), len(coinsB)
 	for {
 		if indexA == lenA {
 			if indexB == lenB {
 				return sum
-			} else {
-				return append(sum, coinsB[indexB:]...)
 			}
+			return append(sum, coinsB[indexB:]...)
 		} else if indexB == lenB {
-			return append(sum, coinsA[indexA:]...)
+			return append(sum, coins[indexA:]...)
 		}
-		coinA, coinB := coinsA[indexA], coinsB[indexB]
+		coinA, coinB := coins[indexA], coinsB[indexB]
 		switch strings.Compare(coinA.Denom, coinB.Denom) {
 		case -1:
 			sum = append(sum, coinA)
-			indexA += 1
+			indexA++
 		case 0:
 			if coinA.Amount+coinB.Amount == 0 {
 				// ignore 0 sum coin type
@@ -138,16 +101,17 @@ func (coinsA Coins) Plus(coinsB Coins) Coins {
 					Amount: coinA.Amount + coinB.Amount,
 				})
 			}
-			indexA += 1
-			indexB += 1
+			indexA++
+			indexB++
 		case 1:
 			sum = append(sum, coinB)
-			indexB += 1
+			indexB++
 		}
 	}
 	return sum
 }
 
+// Negative returns a set of coins with all amount negative
 func (coins Coins) Negative() Coins {
 	res := make([]Coin, 0, len(coins))
 	for _, coin := range coins {
@@ -159,34 +123,42 @@ func (coins Coins) Negative() Coins {
 	return res
 }
 
-func (coinsA Coins) Minus(coinsB Coins) Coins {
-	return coinsA.Plus(coinsB.Negative())
+// Minus subtracts a set of coins from another (adds the inverse)
+func (coins Coins) Minus(coinsB Coins) Coins {
+	return coins.Plus(coinsB.Negative())
 }
 
-func (coinsA Coins) IsGTE(coinsB Coins) bool {
-	diff := coinsA.Minus(coinsB)
+// IsGTE returns True iff coins is NonNegative(), and for every
+// currency in coinsB, the currency is present at an equal or greater
+// amount in coinsB
+func (coins Coins) IsGTE(coinsB Coins) bool {
+	diff := coins.Minus(coinsB)
 	if len(diff) == 0 {
 		return true
 	}
 	return diff.IsNonnegative()
 }
 
+// IsZero returns true if there are no coins
 func (coins Coins) IsZero() bool {
 	return len(coins) == 0
 }
 
-func (coinsA Coins) IsEqual(coinsB Coins) bool {
-	if len(coinsA) != len(coinsB) {
+// IsEqual returns true if the two sets of Coins have the same value
+func (coins Coins) IsEqual(coinsB Coins) bool {
+	if len(coins) != len(coinsB) {
 		return false
 	}
-	for i := 0; i < len(coinsA); i++ {
-		if coinsA[i] != coinsB[i] {
+	for i := 0; i < len(coins); i++ {
+		if coins[i] != coinsB[i] {
 			return false
 		}
 	}
 	return true
 }
 
+// IsPositive returns true if there is at least one coin, and all
+// currencies have a positive value
 func (coins Coins) IsPositive() bool {
 	if len(coins) == 0 {
 		return false
@@ -199,6 +171,8 @@ func (coins Coins) IsPositive() bool {
 	return true
 }
 
+// IsNonnegative returns true if there is no currency with a negative value
+// (even no coins is true here)
 func (coins Coins) IsNonnegative() bool {
 	if len(coins) == 0 {
 		return true
@@ -211,9 +185,23 @@ func (coins Coins) IsNonnegative() bool {
 	return true
 }
 
-/*** Implement Sort interface ***/
+//----------------------------------------
+// Sort interface
 
-func (c Coins) Len() int           { return len(c) }
-func (c Coins) Less(i, j int) bool { return c[i].Denom < c[j].Denom }
-func (c Coins) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
-func (c Coins) Sort()              { sort.Sort(c) }
+//nolint
+func (coins Coins) Len() int           { return len(coins) }
+func (coins Coins) Less(i, j int) bool { return coins[i].Denom < coins[j].Denom }
+func (coins Coins) Swap(i, j int)      { coins[i], coins[j] = coins[j], coins[i] }
+
+var _ sort.Interface = Coins{}
+
+// Sort is a helper function to sort the set of coins inplace
+func (coins Coins) Sort() { sort.Sort(coins) }
+
+//----------------------------------------
+// Misc
+
+type Coinser interface {
+	GetCoins() Coins
+	SetCoins(Coins)
+}
---
+++ x/coinstore/errors.go
@@ -0,0 +1,74 @@
+//nolint
+package coin
+
+import (
+	"fmt"
+
+	"github.com/tepleton/tepleton-sdk/errors"
+)
+
+const (
+	// Coin errors reserve 100 ~ 199.
+	CodeInvalidInput   uint32 = 101
+	CodeInvalidOutput  uint32 = 102
+	CodeInvalidAddress uint32 = 103
+	CodeUnknownAddress uint32 = 103
+	CodeUnknownRequest uint32 = errors.CodeUnknownRequest
+)
+
+// NOTE: Don't stringer this, we'll put better messages in later.
+func codeToDefaultLog(code uint32) string {
+	switch code {
+	case CodeInvalidInput:
+		return "Invalid input coins"
+	case CodeInvalidOutput:
+		return "Invalid output coins"
+	case CodeInvalidAddress:
+		return "Invalid address"
+	case CodeUnknownAddress:
+		return "Unknown address"
+	case CodeUnknownRequest:
+		return "Unknown request"
+	default:
+		return errors.CodeToDefaultLog(code)
+	}
+}
+
+//----------------------------------------
+// Error constructors
+
+func ErrInvalidInput(log string) error {
+	return newError(CodeInvalidInput, log)
+}
+
+func ErrInvalidOutput(log string) error {
+	return newError(CodeInvalidOutput, log)
+}
+
+func ErrInvalidAddress(log string) error {
+	return newError(CodeInvalidAddress, log)
+}
+
+func ErrUnknownAddress(log string) error {
+	return newError(CodeUnknownAddress, log)
+}
+
+func ErrUnknownRequest(log string) error {
+	return newError(CodeUnknownRequest, log)
+}
+
+//----------------------------------------
+// Misc
+
+func logOrDefault(log string, code uint32) string {
+	if log != "" {
+		return log
+	} else {
+		return codeToDefaultLog
+	}
+}
+
+func newError(code uint32, log string) error {
+	log = logOrDefaultLog(log, code)
+	return errors.NewWRSPError(code, log)
+}
