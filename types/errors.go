df264d4bf0513a468e08dccc045d6bfab6a67943
camelfred <camel_web@outlook.com>
2019-09-29 13:32:26 -0800

    [add] new error api

--- types/errors.go
+++ types/errors.go
@@ -1,22 +1,165 @@
 package types
 
 import (
-	wrsp "github.com/tepleton/wrsp/types"
+	"fmt"
 )
 
-var (
-	ErrInternalError        = wrsp.NewError(wrsp.CodeType_InternalError, "Internal error")
-	ErrDuplicateAddress     = wrsp.NewError(wrsp.CodeType_BaseDuplicateAddress, "Error duplicate address")
-	ErrEncodingError        = wrsp.NewError(wrsp.CodeType_BaseEncodingError, "Error encoding error")
-	ErrInsufficientFees     = wrsp.NewError(wrsp.CodeType_BaseInsufficientFees, "Error insufficient fees")
-	ErrInsufficientFunds    = wrsp.NewError(wrsp.CodeType_BaseInsufficientFunds, "Error insufficient funds")
-	ErrInsufficientGasPrice = wrsp.NewError(wrsp.CodeType_BaseInsufficientGasPrice, "Error insufficient gas price")
-	ErrInvalidAddress       = wrsp.NewError(wrsp.CodeType_BaseInvalidAddress, "Error invalid address")
-	ErrInvalidAmount        = wrsp.NewError(wrsp.CodeType_BaseInvalidAmount, "Error invalid amount")
-	ErrInvalidPubKey        = wrsp.NewError(wrsp.CodeType_BaseInvalidPubKey, "Error invalid pubkey")
-	ErrInvalidSequence      = wrsp.NewError(wrsp.CodeType_BaseInvalidSequence, "Error invalid sequence")
-	ErrInvalidSignature     = wrsp.NewError(wrsp.CodeType_BaseInvalidSignature, "Error invalid signature")
-	ErrUnknownPubKey        = wrsp.NewError(wrsp.CodeType_BaseUnknownPubKey, "Error unknown pubkey")
-
-	ResultOK = wrsp.NewResultOK(nil, "")
+const (
+	// WRSP Response Codes
+	// Base SDK reserves 0 ~ 99.
+	CodeInternalError       uint32 = 1
+	CodeTxParseError               = 2
+	CodeBadNonce                   = 3
+	CodeUnauthorized               = 4
+	CodeInsufficientFunds          = 5
+	CodeUnknownRequest             = 6
+	CodeUnrecognizedAddress        = 7
 )
+
+// NOTE: Don't stringer this, we'll put better messages in later.
+func CodeToDefaultLog(code uint32) string {
+	switch code {
+	case CodeInternalError:
+		return "Internal error"
+	case CodeTxParseError:
+		return "Tx parse error"
+	case CodeBadNonce:
+		return "Bad nonce"
+	case CodeUnauthorized:
+		return "Unauthorized"
+	case CodeInsufficientFunds:
+		return "Insufficent funds"
+	case CodeUnknownRequest:
+		return "Unknown request"
+	case CodeUnrecognizedAddress:
+		return "Unrecognized address"
+	default:
+		return fmt.Sprintf("Unknown code %d", code)
+	}
+}
+
+//--------------------------------------------------------------------------------
+// All errors are created via constructors so as to enable us to hijack them
+// and inject stack traces if we really want to.
+
+func InternalError(log string) Error {
+	return newError(CodeInternalError, log)
+}
+
+func TxParseError(log string) Error {
+	return newError(CodeTxParseError, log)
+}
+
+func BadNonce(log string) Error {
+	return newError(CodeBadNonce, log)
+}
+
+func Unauthorized(log string) Error {
+	return newError(CodeUnauthorized, log)
+}
+
+func InsufficientFunds(log string) Error {
+	return newError(CodeInsufficientFunds, log)
+}
+
+func UnknownRequest(log string) Error {
+	return newError(CodeUnknownRequest, log)
+}
+
+func UnrecognizedAddress(log string) Error {
+	return newError(CodeUnrecognizedAddress, log)
+}
+
+//----------------------------------------
+// Error & sdkError
+
+type Error interface {
+	Error() string
+	WRSPCode() uint32
+	WRSPLog() string
+	Trace(msg string) Error
+	TraceCause(cause error, msg string) Error
+	Cause() error
+	Result() Result
+}
+
+func NewError(code uint32, log string) Error {
+	return newError(code, log)
+}
+
+type traceItem struct {
+	msg      string
+	filename string
+	lineno   int
+}
+
+type sdkError struct {
+	code  uint32
+	log   string
+	cause error
+	trace []traceItem
+}
+
+func newError(code uint32, log string) *sdkError {
+	// TODO capture stacktrace if ENV is set.
+	if log == "" {
+		log = CodeToDefaultLog(code)
+	}
+	return &sdkError{
+		code:  code,
+		log:   log,
+		cause: nil,
+		trace: nil,
+	}
+}
+
+// Implements WRSPError.
+func (err *sdkError) Error() string {
+	return fmt.Sprintf("Error{%d:%s,%v,%v}", err.code, err.log, err.cause, len(err.trace))
+}
+
+// Implements WRSPError.
+func (err *sdkError) WRSPCode() uint32 {
+	return err.code
+}
+
+// Implements WRSPError.
+func (err *sdkError) WRSPLog() string {
+	return err.log
+}
+
+// Add tracing information to log with msg.
+func (err *sdkError) Trace(msg string) Error {
+	// Include file & line number & msg to log.
+	// Do not include the whole stack trace.
+	err.trace = append(err.trace, traceItem{
+		filename: "todo", // TODO
+		lineno:   -1,     // TODO
+		msg:      msg,
+	})
+	return err
+}
+
+// Add tracing information to log with cause and msg.
+func (err *sdkError) TraceCause(cause error, msg string) Error {
+	err.cause = cause
+	// Include file & line number & cause & msg to log.
+	// Do not include the whole stack trace.
+	err.trace = append(err.trace, traceItem{
+		filename: "todo", // TODO
+		lineno:   -1,     // TODO
+		msg:      msg,
+	})
+	return err
+}
+
+func (err *sdkError) Cause() error {
+	return err.cause
+}
+
+func (err *sdkError) Result() Result {
+	return Result{
+		Code: err.WRSPCode(),
+		Log:  err.WRSPLog(),
+	}
+}
