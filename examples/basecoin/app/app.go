bf3098494cce0534ff069c09da7255a6be1a259a
camelfred <camel_web@outlook.com>
2019-09-29 17:25:45 -0800

    [fix] codecs in basecoin, and add more testcases

---
+++ examples/basecoin/app/app.go
@@ -0,0 +1,98 @@
+package app
+
+import (
+	"fmt"
+	"os"
+
+	apm "github.com/tepleton/tepleton-sdk/app"
+	sdk "github.com/tepleton/tepleton-sdk/types"
+	"github.com/tepleton/wrsp/server"
+	"github.com/tepleton/go-wire"
+	cmn "github.com/tepleton/tmlibs/common"
+)
+
+const appName = "BasecoinApp"
+
+type BasecoinApp struct {
+	*apm.App
+	cdc        *wire.Codec
+	multiStore sdk.CommitMultiStore
+
+	// The key to access the substores.
+	mainStoreKey *sdk.KVStoreKey
+	ibcStoreKey  *sdk.KVStoreKey
+
+	// Additional stores:
+	accStore sdk.AccountStore
+}
+
+// TODO: This should take in more configuration options.
+func NewBasecoinApp() *BasecoinApp {
+
+	// Create and configure app.
+	var app = &BasecoinApp{}
+	app.initKeys()
+	app.initMultiStore()
+	app.initAppStore()
+	app.initSDKApp()
+	app.initCodec()
+	app.initTxDecoder()
+	app.initAnteHandler()
+	app.initRoutes()
+
+	// TODO: Load genesis
+	// TODO: InitChain with validators
+	// TODO: Set the genesis accounts
+	app.loadStores()
+
+	return app
+}
+
+func (app *BasecoinApp) RunForever() {
+
+	// Start the WRSP server
+	srv, err := server.NewServer("0.0.0.0:46658", "socket", app)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	srv.Start()
+
+	// Wait forever
+	cmn.TrapSignal(func() {
+		// Cleanup
+		srv.Stop()
+	})
+
+}
+
+func (app *BasecoinApp) initKeys() {
+	app.mainStoreKey = sdk.NewKVStoreKey("main")
+	app.ibcStoreKey = sdk.NewKVStoreKey("ibc")
+}
+
+// depends on initMultiStore()
+func (app *BasecoinApp) initSDKApp() {
+	app.App = apm.NewApp(appName, app.multiStore)
+}
+
+func (app *BasecoinApp) initCodec() {
+	app.cdc = wire.NewCodec()
+	app.registerMsgs()
+}
+
+// depends on initSDKApp()
+func (app *BasecoinApp) initTxDecoder() {
+	app.App.SetTxDecoder(app.decodeTx)
+}
+
+// initAnteHandler defined in app/routes.go
+// initRoutes defined in app/routes.go
+
+// Load the stores.
+func (app *BasecoinApp) loadStores() {
+	if err := app.LoadLatestVersion(app.mainStoreKey); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
