655dabea2476feb7bec66ad5c60f147ec63ab696
camelfred <camel_web@outlook.com>
2019-09-29 20:54:20 -0800

    [add] baseaccount

---
+++ examples/basecoin/app/init_stores.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tepleton/tepleton-sdk/examples/basecoin/types"
+	"github.com/tepleton/tepleton-sdk/store"
+	"github.com/tepleton/tepleton-sdk/x/auth"
+	dbm "github.com/tepleton/tmlibs/db"
+)
+
+// initStores() happens after initCapKeys(), but before initSDKApp() and initRoutes().
+func (app *BasecoinApp) initStores() {
+	app.initMultiStore()
+	app.initAccountStore()
+}
+
+// Initialize root MultiStore.
+func (app *BasecoinApp) initMultiStore() {
+
+	// Create the underlying leveldb datastore which will
+	// persist the Merkle tree inner & leaf nodes.
+	db, err := dbm.NewGoLevelDB("basecoin", "basecoin-data")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Create CommitStoreLoader.
+	cacheSize := 10000
+	numHistory := int64(100)
+	mainLoader := store.NewIAVLStoreLoader(db, cacheSize, numHistory)
+	ibcLoader := store.NewIAVLStoreLoader(db, cacheSize, numHistory)
+
+	// Create MultiStore
+	multiStore := store.NewCommitMultiStore(db)
+	multiStore.SetSubstoreLoader(app.mainStoreKey, mainLoader)
+	multiStore.SetSubstoreLoader(app.ibcStoreKey, ibcLoader)
+
+	// Finally,
+	app.multiStore = multiStore
+}
+
+// Initialize the AccountStore, which accesses the MultiStore.
+func (app *BasecoinApp) initAccountStore() {
+	accStore := auth.NewAccountStore(
+		// where accounts are persisted in the MultiStore.
+		app.mainStoreKey,
+		// prototype sdk.Account.
+		&types.AppAccount{},
+	)
+
+	// If there are additional interfaces & concrete types that
+	// need to be registered w/ wire.Codec, they can be registered
+	// here before the accStore is sealed.
+	cdc := accStore.WireCodec()
+	auth.RegisterWireBaseAccount(cdc)
+
+	app.accStore = accStore.Seal()
+}
---
+++ x/auth/baseaccount_test.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"testing"
+
+	sdk "github.com/tepleton/tepleton-sdk/types"
+	"github.com/stretchr/testify/assert"
+	crypto "github.com/tepleton/go-crypto"
+	wire "github.com/tepleton/go-wire"
+)
+
+func TestBaseAccount(t *testing.T) {
+	key := crypto.GenPrivKeyEd25519()
+	pub := key.PubKey()
+	addr := pub.Address()
+	someCoins := sdk.Coins{{"atom", 123}, {"eth", 246}}
+	seq := int64(7)
+
+	acc := NewBaseAccountWithAddress(addr)
+
+	// need a codec for marshaling
+	codec := wire.NewCodec()
+	crypto.RegisterWire(codec)
+
+	err := acc.SetPubKey(pub)
+	assert.Nil(t, err)
+	assert.Equal(t, pub, acc.GetPubKey())
+
+	assert.Equal(t, addr, acc.GetAddress())
+
+	err = acc.SetCoins(someCoins)
+	assert.Nil(t, err)
+	assert.Equal(t, someCoins, acc.GetCoins())
+
+	err = acc.SetSequence(seq)
+	assert.Nil(t, err)
+	assert.Equal(t, seq, acc.GetSequence())
+
+	b, err := codec.MarshalBinary(acc)
+	assert.Nil(t, err)
+
+	var acc2 BaseAccount
+	err = codec.UnmarshalBinary(b, &acc2)
+	assert.Nil(t, err)
+	assert.Equal(t, acc, acc2)
+
+	acc2 = BaseAccount{}
+	err = codec.UnmarshalBinary(b[:len(b)/2], &acc2)
+	assert.NotNil(t, err)
+}
