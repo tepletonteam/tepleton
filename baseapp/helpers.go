48d68a55852ef23f5bf8ef5eeee814f7c15773de
moon <mooncakeeos@yahoo.co.jp>
2019-10-04 16:41:20 -0800

    [refactor] basecoin

---
+++ baseapp/helpers.go
@@ -0,0 +1,24 @@
+package baseapp
+
+import (
+	"github.com/tepleton/wrsp/server"
+	wrsp "github.com/tepleton/wrsp/types"
+	cmn "github.com/tepleton/tmlibs/common"
+)
+
+// RunForever - BasecoinApp execution and cleanup
+func RunForever(app wrsp.Application) {
+
+	// Start the WRSP server
+	srv, err := server.NewServer("0.0.0.0:46658", "socket", app)
+	if err != nil {
+		cmn.Exit(err.Error())
+	}
+	srv.Start()
+
+	// Wait forever
+	cmn.TrapSignal(func() {
+		// Cleanup
+		srv.Stop()
+	})
+}
--- examples/basecoin/cmd/basecoind/main.go
+++ examples/basecoin/cmd/basecoind/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/tepleton/tepleton-sdk/examples/basecoin/app"
+import (
+	"github.com/tepleton/tepleton-sdk/baseapp"
+	"github.com/tepleton/tepleton-sdk/examples/basecoin/app"
+)
 
 func main() {
 	// TODO CREATE CLI
 
 	bapp := app.NewBasecoinApp("")
-	bapp.RunForever()
+	baseapp.RunForever(bapp)
 }
