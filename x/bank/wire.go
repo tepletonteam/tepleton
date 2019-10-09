3f6f52cf1759fd3b4fc003409b52b05dcb036e6c
camelfred <camel_web@outlook.com>
2019-10-09 12:10:19 -0800

    [refactor] update wire

--- x/bank/wire.go
+++ x/bank/wire.go
@@ -1,6 +1,7 @@
 package bank
 
 import (
+	sdk "github.com/tepleton/tepleton-sdk/types"
 	crypto "github.com/tepleton/go-crypto"
 	"github.com/tepleton/go-wire"
 )
@@ -9,5 +10,8 @@ func RegisterWire(cdc *wire.Codec) {
 	// TODO include option to always include prefix bytes.
 	cdc.RegisterConcrete(SendMsg{}, "tepleton-sdk/SendMsg", nil)
 	cdc.RegisterConcrete(IssueMsg{}, "tepleton-sdk/IssueMsg", nil)
+
+	cdc.RegisterInterface((*sdk.Msg)(nil), nil)
+
 	crypto.RegisterWire(cdc) // Register crypto.[PubKey,PrivKey,Signature] types.
 }
