d2a6252b0fac5ba483d6d0628e28d82694c192ff
tepleton <tepleton@outlook.com>
2019-10-16 07:58:17 -0800

    [refactor] recover test file

--- x/ibc/ibc_test.go
+++ x/ibc/ibc_test.go
@@ -7,11 +7,19 @@ import (
 
 	wrsp "github.com/tepleton/wrsp/types"
 	"github.com/tepleton/go-crypto"
+	oldwire "github.com/tepleton/go-wire"
 	dbm "github.com/tepleton/tmlibs/db"
 
 	"github.com/tepleton/tepleton-sdk/store"
 	sdk "github.com/tepleton/tepleton-sdk/types"
+	"github.com/tepleton/tepleton-sdk/wire"
+	"github.com/tepleton/tepleton-sdk/x/auth"
+	"github.com/tepleton/tepleton-sdk/x/bank"
+
+	"github.com/tepleton/tepleton-sdk/examples/basecoin/x/cool"
 )
+
+// AccountMapper(/CoinKeeper) and IBCMapper should use different StoreKey later
 
 func defaultContext(key sdk.StoreKey) sdk.Context {
 	db := dbm.NewMemDB()
@@ -26,22 +34,70 @@ func newAddress() crypto.Address {
 	return crypto.GenPrivKeyEd25519().PubKey().Address()
 }
 
-func TestHandler(t *testing.T) {
+func getCoins(ck bank.CoinKeeper, ctx sdk.Context, addr crypto.Address) (sdk.Coins, sdk.Error) {
+	zero := sdk.Coins{}
+	return ck.AddCoins(ctx, addr, zero)
+}
+
+// custom tx codec
+// TODO: use new go-wire
+func makeCodec() *wire.Codec {
+
+	const msgTypeSend = 0x1
+	const msgTypeIssue = 0x2
+	const msgTypeQuiz = 0x3
+	const msgTypeSetTrend = 0x4
+	const msgTypeIBCTransferMsg = 0x5
+	const msgTypeIBCReceiveMsg = 0x6
+	var _ = oldwire.RegisterInterface(
+		struct{ sdk.Msg }{},
+		oldwire.ConcreteType{bank.SendMsg{}, msgTypeSend},
+		oldwire.ConcreteType{bank.IssueMsg{}, msgTypeIssue},
+		oldwire.ConcreteType{cool.QuizMsg{}, msgTypeQuiz},
+		oldwire.ConcreteType{cool.SetTrendMsg{}, msgTypeSetTrend},
+		oldwire.ConcreteType{IBCTransferMsg{}, msgTypeIBCTransferMsg},
+		oldwire.ConcreteType{IBCReceiveMsg{}, msgTypeIBCReceiveMsg},
+	)
+
+	const accTypeApp = 0x1
+	var _ = oldwire.RegisterInterface(
+		struct{ sdk.Account }{},
+		oldwire.ConcreteType{&auth.BaseAccount{}, accTypeApp},
+	)
+	cdc := wire.NewCodec()
+
+	// cdc.RegisterInterface((*sdk.Msg)(nil), nil)
+	// bank.RegisterWire(cdc)   // Register bank.[SendMsg,IssueMsg] types.
+	// crypto.RegisterWire(cdc) // Register crypto.[PubKey,PrivKey,Signature] types.
+	// ibc.RegisterWire(cdc) // Register ibc.[IBCTransferMsg, IBCReceiveMsg] types.
+	return cdc
+}
+
+func TestIBC(t *testing.T) {
+	var _ = makeCodec()
+
 	key := sdk.NewKVStoreKey("ibc")
 	ctx := defaultContext(key)
-	ibcm := NewIBCMapper(key)
 
-	h := NewHandler(ibcm)
+	am := auth.NewAccountMapper(key, &auth.BaseAccount{})
+	ck := bank.NewCoinKeeper(am)
 
 	src := newAddress()
 	dest := newAddress()
 	chainid := "ibcchain"
-	coin := sdk.Coin{Denom: "neutron", Amount: 10000}
+	zero := sdk.Coins{}
+	mycoins := sdk.Coins{sdk.Coin{"mycoin", 10}}
 
+	coins, err := ck.AddCoins(ctx, src, mycoins)
+	assert.Nil(t, err)
+	assert.Equal(t, mycoins, coins)
+
+	ibcm := NewIBCMapper(key)
+	h := NewHandler(ibcm, ck)
 	packet := IBCPacket{
 		SrcAddr:   src,
 		DestAddr:  dest,
-		Coins:     sdk.Coins{coin},
+		Coins:     mycoins,
 		SrcChain:  chainid,
 		DestChain: chainid,
 	}
@@ -62,6 +118,10 @@ func TestHandler(t *testing.T) {
 	res = h(ctx, msg)
 	assert.True(t, res.IsOK())
 
+	coins, err = getCoins(ck, ctx, src)
+	assert.Nil(t, err)
+	assert.Equal(t, zero, coins)
+
 	egl = ibcm.getEgressLength(store, chainid)
 	assert.Equal(t, egl, int64(1))
 
@@ -75,6 +135,10 @@ func TestHandler(t *testing.T) {
 	}
 	res = h(ctx, msg)
 	assert.True(t, res.IsOK())
+
+	coins, err = getCoins(ck, ctx, dest)
+	assert.Nil(t, err)
+	assert.Equal(t, mycoins, coins)
 
 	igs = ibcm.GetIngressSequence(ctx, chainid)
 	assert.Equal(t, igs, int64(1))
