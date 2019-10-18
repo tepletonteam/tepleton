c50ba0ec2112045d91c544f80b7f4ab9597dcd3a
camelfred <camel_web@outlook.com>
2019-10-17 20:50:22 -0800

    [add] staking

---
+++ x/staking/keeper_test.go
@@ -0,0 +1,77 @@
+package staking
+
+import (
+	"fmt"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	wrsp "github.com/tepleton/wrsp/types"
+	crypto "github.com/tepleton/go-crypto"
+	dbm "github.com/tepleton/tmlibs/db"
+
+	"github.com/tepleton/tepleton-sdk/store"
+	sdk "github.com/tepleton/tepleton-sdk/types"
+	"github.com/tepleton/tepleton-sdk/x/bank"
+)
+
+func setupMultiStore() (sdk.MultiStore, *sdk.KVStoreKey) {
+	db := dbm.NewMemDB()
+	capKey := sdk.NewKVStoreKey("capkey")
+	ms := store.NewCommitMultiStore(db)
+	ms.MountStoreWithDB(capKey, sdk.StoreTypeIAVL, db)
+	ms.LoadLatestVersion()
+	return ms, capKey
+}
+
+func TestKeeperGetSet(t *testing.T) {
+	ms, capKey := setupMultiStore()
+
+	ctx := sdk.NewContext(ms, wrsp.Header{}, false, nil)
+	stakeKeeper := NewKeeper(capKey, bank.NewCoinKeeper(nil))
+	addr := sdk.Address([]byte("some-address"))
+
+	bi := stakeKeeper.getBondInfo(ctx, addr)
+	assert.Equal(t, bi, bondInfo{})
+
+	privKey := crypto.GenPrivKeyEd25519()
+
+	bi = bondInfo{
+		PubKey: privKey.PubKey(),
+		Power:  int64(10),
+	}
+	fmt.Printf("Pubkey: %v\n", privKey.PubKey())
+	stakeKeeper.setBondInfo(ctx, addr, bi)
+
+	savedBi := stakeKeeper.getBondInfo(ctx, addr)
+	assert.NotNil(t, savedBi)
+	fmt.Printf("Bond Info: %v\n", savedBi)
+	assert.Equal(t, int64(10), savedBi.Power)
+}
+
+func TestBonding(t *testing.T) {
+	ms, capKey := setupMultiStore()
+
+	ctx := sdk.NewContext(ms, wrsp.Header{}, false, nil)
+	stakeKeeper := NewKeeper(capKey, bank.NewCoinKeeper(nil))
+	addr := sdk.Address([]byte("some-address"))
+	privKey := crypto.GenPrivKeyEd25519()
+	pubKey := privKey.PubKey()
+
+	_, _, err := stakeKeeper.Unbond(ctx, addr)
+	assert.Equal(t, err, ErrInvalidUnbond())
+
+	_, err = stakeKeeper.Bond(ctx, addr, pubKey, 10)
+	assert.Nil(t, err)
+
+	power, err := stakeKeeper.Bond(ctx, addr, pubKey, 10)
+	assert.Equal(t, int64(20), power)
+
+	pk, _, err := stakeKeeper.Unbond(ctx, addr)
+	assert.Nil(t, err)
+	assert.Equal(t, pubKey, pk)
+
+	_, _, err = stakeKeeper.Unbond(ctx, addr)
+	assert.Equal(t, err, ErrInvalidUnbond())
+}
