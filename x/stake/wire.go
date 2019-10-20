39919f926ad6975b3355ff49d1713224fa402a65
Stevilo <stefanzan007@outlook.com>
2019-10-20 11:11:39 -0800

    [refactor] change handler

---
+++ x/stake/wire.go
@@ -0,0 +1,12 @@
+package bank
+
+import (
+	"github.com/tepleton/go-wire"
+)
+
+// XXX complete
+func RegisterWire(cdc *wire.Codec) {
+	// TODO include option to always include prefix bytes.
+	//cdc.RegisterConcrete(SendMsg{}, "tepleton-sdk/SendMsg", nil)
+	//cdc.RegisterConcrete(IssueMsg{}, "tepleton-sdk/IssueMsg", nil)
+}
