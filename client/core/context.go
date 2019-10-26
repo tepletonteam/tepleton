6354809d2f10ddc3b1b308b9c866350a0169fe4b
tepleton <tepleton@outlook.com>
2019-10-26 09:18:32 -0800

    [refactor] change CoreContext parameters

---
+++ client/core/context.go
@@ -0,0 +1,60 @@
+package core
+
+import (
+	// "fmt"
+
+	// "github.com/pkg/errors"
+	"github.com/spf13/viper"
+
+	"github.com/tepleton/tepleton-sdk/client"
+)
+
+type CoreContext struct {
+	ChainID         string
+	Height          int64
+	TrustNode       bool
+	NodeURI         string
+	FromAddressName string
+	Sequence        int64
+}
+
+func NewCoreContextFromViper() CoreContext {
+	return CoreContext{
+		ChainID:         viper.GetString(client.FlagChainID),
+		Height:          viper.GetInt64(client.FlagHeight),
+		TrustNode:       viper.GetBool(client.FlagTrustNode),
+		NodeURI:         viper.GetString(client.FlagNode),
+		FromAddressName: viper.GetString(client.FlagName),
+		Sequence:        viper.GetInt64(client.FlagSequence),
+	}
+}
+
+func (c CoreContext) WithChainID(chainID string) CoreContext {
+	c.ChainID = chainID
+	return c
+}
+
+func (c CoreContext) WithHeight(height int64) CoreContext {
+	c.Height = height
+	return c
+}
+
+func (c CoreContext) WithTrustNode(trustNode bool) CoreContext {
+	c.TrustNode = trustNode
+	return c
+}
+
+func (c CoreContext) WithNodeURI(nodeURI string) CoreContext {
+	c.NodeURI = nodeURI
+	return c
+}
+
+func (c CoreContext) WithFromAddressName(fromAddressName string) CoreContext {
+	c.FromAddressName = fromAddressName
+	return c
+}
+
+func (c CoreContext) WithSequence(sequence int64) CoreContext {
+	c.Sequence = sequence
+	return c
+}
