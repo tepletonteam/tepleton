91f916a9f27da24a0652c29eb7896171e633f337
camelfred <camel_web@outlook.com>
2019-11-05 11:35:22 -0900

    [refactor] init genesis, write genesis

--- cmd/ton/app/app.go
+++ cmd/ton/app/app.go
@@ -113,7 +113,7 @@ func (app *GaiaApp) initChainer(ctx sdk.Context, req wrsp.RequestInitChain) wrsp
 	}
 
 	// load the initial stake information
-	app.stakeKeeper.InitGenesis(ctx, genesisState.StakeData)
+	stake.InitGenesis(app.stakeKeeper, ctx, genesisState.StakeData)
 
 	return wrsp.ResponseInitChain{}
 }
@@ -122,6 +122,6 @@ func (app *GaiaApp) initChainer(ctx sdk.Context, req wrsp.RequestInitChain) wrsp
 func (app *GaiaApp) ExportGenesis() GenesisState {
 	return GenesisState{
 		Accounts:  []GenesisAccount{},
-		StakeData: app.stakeKeeper.WriteGenesis(app.NewContext(true, wrsp.Header{})),
+		StakeData: stake.WriteGenesis(app.stakeKeeper, app.NewContext(true, wrsp.Header{})),
 	}
 }
