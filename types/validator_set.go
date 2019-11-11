d6d29bc01024408145e0c9a309c5335e00512fe0
camelfred <camel_web@outlook.com>
2019-11-10 20:41:17 -0900

    [add] ValidatorSet, and remove methods

--- types/validator_set.go
+++ types/validator_set.go
@@ -2,15 +2,29 @@ package types
 
 import (
 	wrsp "github.com/tepleton/wrsp/types"
+	"github.com/tepleton/go-crypto"
 )
 
-type Validator = wrsp.Validator
+type Validator interface {
+	GetAddress() Address
+	GetPubKey() crypto.PubKey
+	GetPower() Rat
+}
+
+func WRSPValidator(v Validator) wrsp.Validator {
+	return wrsp.Validator{
+		PubKey: v.GetPubKey().Bytes(),
+		Power:  v.GetPower().Evaluate(),
+	}
+}
+
+type ValidatorSet interface {
+	Iterate(func(int, Validator))
+	Size() int
+}
 
 type ValidatorSetKeeper interface {
-	Validators(Context) []*Validator
-	Size(Context) int
-	IsValidator(Context, Address) bool
-	GetByAddress(Context, Address) (int, *Validator)
-	GetByIndex(Context, int) *Validator
+	ValidatorSet(Context) ValidatorSet
+	GetByAddress(Context, Address) Validator
 	TotalPower(Context) Rat
 }
