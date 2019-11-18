d8e014415d42b4a92e5c62b04ad353b72552cd86
camelfred <camel_web@outlook.com>
2019-11-17 17:52:18 -0900

    [refactor] remove absent validators

--- x/auth/mapper_test.go
+++ x/auth/mapper_test.go
@@ -31,7 +31,7 @@ func TestAccountMapperGetSet(t *testing.T) {
 	RegisterBaseAccount(cdc)
 
 	// make context and mapper
-	ctx := sdk.NewContext(ms, wrsp.Header{}, false, nil, log.NewNopLogger(), nil)
+	ctx := sdk.NewContext(ms, wrsp.Header{}, false, nil, log.NewNopLogger())
 	mapper := NewAccountMapper(cdc, capKey, &BaseAccount{})
 
 	addr := sdk.Address([]byte("some-address"))
