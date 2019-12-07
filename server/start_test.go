a3ce4bd8d607761c301f466a34cba83d733c259b
tepleton <tepleton@outlook.com>
2019-12-07 22:50:18 +0800

    [refactor] Merge pull request #1509 from cosmos/bez/1269-basecoind-start-failure

--- server/start_test.go
+++ server/start_test.go
@@ -6,13 +6,14 @@ import (
 	"testing"
 	"time"
 
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/require"
 
 	"github.com/tepleton/tepleton-sdk/server/mock"
 	"github.com/tepleton/tepleton-sdk/wire"
 	"github.com/tepleton/tepleton/wrsp/server"
 	tcmd "github.com/tepleton/tepleton/cmd/tepleton/commands"
-	"github.com/tepleton/tepleton/libs/log"
+	"github.com/tepleton/tmlibs/log"
 )
 
 func TestStartStandAlone(t *testing.T) {
@@ -44,9 +45,37 @@ func TestStartStandAlone(t *testing.T) {
 	svr.SetLogger(logger.With("module", "wrsp-server"))
 	svr.Start()
 
-	timer := time.NewTimer(time.Duration(2) * time.Second)
+	timer := time.NewTimer(time.Duration(5) * time.Second)
 	select {
 	case <-timer.C:
 		svr.Stop()
 	}
+}
+
+func TestStartWithTendermint(t *testing.T) {
+	defer setupViper(t)()
+
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout)).
+		With("module", "mock-cmd")
+	cfg, err := tcmd.ParseConfig()
+	require.Nil(t, err)
+	ctx := NewContext(cfg, logger)
+	cdc := wire.NewCodec()
+	appInit := AppInit{
+		AppGenState: mock.AppGenState,
+		AppGenTx:    mock.AppGenTx,
+	}
+	initCmd := InitCmd(ctx, cdc, appInit)
+	err = initCmd.RunE(nil, nil)
+	require.NoError(t, err)
+
+	// set up app and start up
+	viper.Set(flagWithTendermint, true)
+	startCmd := StartCmd(ctx, mock.NewApp)
+	svrAddr, _, err := FreeTCPAddr()
+	require.NoError(t, err)
+	startCmd.Flags().Set(flagAddress, svrAddr) // set to a new free address
+	timeout := time.Duration(5) * time.Second
+
+	close(RunOrTimeout(startCmd, timeout, t))
 }
