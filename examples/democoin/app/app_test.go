6dbe74bde6fa4d4b07ee751600e23f5f185f76fa
tepleton <tepleton@outlook.com>
2019-12-07 20:24:47 +0800

    [fix] Merge pull request #1509: Fix basecoin Example

--- examples/democoin/app/app_test.go
+++ examples/democoin/app/app_test.go
@@ -14,8 +14,8 @@ import (
 
 	wrsp "github.com/tepleton/tepleton/wrsp/types"
 	"github.com/tepleton/tepleton/crypto"
-	dbm "github.com/tepleton/tepleton/libs/db"
-	"github.com/tepleton/tepleton/libs/log"
+	dbm "github.com/tepleton/tmlibs/db"
+	"github.com/tepleton/tmlibs/log"
 )
 
 func setGenesis(bapp *DemocoinApp, trend string, accs ...auth.BaseAccount) error {
