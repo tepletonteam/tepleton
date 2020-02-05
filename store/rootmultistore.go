8a6c8240f565e75b43e3a46d4dd009801ea46ba4
kikumarneela <kikumarneela@yandex.com>
2020-02-05 17:28:43 +0800

    [add] update for tm v0.22.0. tmlibs->tendermint/libs

--- store/rootmultistore.go
+++ store/rootmultistore.go
@@ -7,8 +7,8 @@ import (
 	"golang.org/x/crypto/ripemd160"
 
 	wrsp "github.com/tepleton/tepleton/wrsp/types"
-	dbm "github.com/tepleton/tmlibs/db"
-	"github.com/tepleton/tmlibs/merkle"
+	dbm "github.com/tepleton/tepleton/libs/db"
+	"github.com/tepleton/tepleton/crypto/merkle"
 
 	sdk "github.com/tepleton/tepleton-sdk/types"
 )
