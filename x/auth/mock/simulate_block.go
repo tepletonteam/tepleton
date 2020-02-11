991376f77b553e799b4dc16c3291f80013d887d9
kikumarneela <kikumarneela@yandex.com>
2020-02-11 18:36:31 +0800

    [add] update dep for tm v0.22.0

--- x/auth/mock/simulate_block.go
+++ x/auth/mock/simulate_block.go
@@ -6,11 +6,10 @@ import (
 	"github.com/tepleton/tepleton-sdk/baseapp"
 	sdk "github.com/tepleton/tepleton-sdk/types"
 	"github.com/tepleton/tepleton-sdk/x/auth"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	crypto "github.com/tepleton/go-crypto"
+	"github.com/tepleton/tepleton/crypto"
 
-	wrsp "github.com/tepleton/wrsp/types"
+	wrsp "github.com/tepleton/tepleton/wrsp/types"
 )
 
 var chainID = "" // TODO
@@ -29,42 +28,64 @@ func SetGenesis(app *App, accs []auth.Account) {
 func CheckBalance(t *testing.T, app *App, addr sdk.Address, exp sdk.Coins) {
 	ctxCheck := app.BaseApp.NewContext(true, wrsp.Header{})
 	res := app.AccountMapper.GetAccount(ctxCheck, addr)
-	assert.Equal(t, exp, res.GetCoins())
+	require.Equal(t, exp, res.GetCoins())
 }
 
 // generate a signed transaction
-func GenTx(msg sdk.Msg, accnums []int64, seq []int64, priv ...crypto.PrivKeyEd25519) auth.StdTx {
+func GenTx(msgs []sdk.Msg, accnums []int64, seq []int64, priv ...crypto.PrivKeyEd25519) auth.StdTx {
 
 	// make the transaction free
 	fee := auth.StdFee{
-		sdk.Coins{{"foocoin", 0}},
+		sdk.Coins{sdk.NewCoin("foocoin", 0)},
 		100000,
 	}
 
 	sigs := make([]auth.StdSignature, len(priv))
+	memo := "testmemotestmemo"
 	for i, p := range priv {
+		sig, err := p.Sign(auth.StdSignBytes(chainID, accnums[i], seq[i], fee, msgs, memo))
+		if err != nil {
+			panic(err)
+		}
 		sigs[i] = auth.StdSignature{
 			PubKey:        p.PubKey(),
-			Signature:     p.Sign(auth.StdSignBytes(chainID, accnums, seq, fee, msg)),
+			Signature:     sig,
 			AccountNumber: accnums[i],
 			Sequence:      seq[i],
 		}
 	}
-	return auth.NewStdTx(msg, fee, sigs)
+	return auth.NewStdTx(msgs, fee, sigs, memo)
+}
+
+// generate a set of signed transactions a msg, that differ only by having the
+// sequence numbers incremented between every transaction.
+func GenSequenceOfTxs(msgs []sdk.Msg, accnums []int64, initSeqNums []int64, numToGenerate int, priv ...crypto.PrivKeyEd25519) []auth.StdTx {
+	txs := make([]auth.StdTx, numToGenerate, numToGenerate)
+	for i := 0; i < numToGenerate; i++ {
+		txs[i] = GenTx(msgs, accnums, initSeqNums, priv...)
+		incrementAllSequenceNumbers(initSeqNums)
+	}
+	return txs
+}
+
+func incrementAllSequenceNumbers(initSeqNums []int64) {
+	for i := 0; i < len(initSeqNums); i++ {
+		initSeqNums[i]++
+	}
 }
 
 // check a transaction result
-func SignCheck(t *testing.T, app *baseapp.BaseApp, msg sdk.Msg, accnums []int64, seq []int64, priv ...crypto.PrivKeyEd25519) sdk.Result {
-	tx := GenTx(msg, accnums, seq, priv...)
+func SignCheck(app *baseapp.BaseApp, msgs []sdk.Msg, accnums []int64, seq []int64, priv ...crypto.PrivKeyEd25519) sdk.Result {
+	tx := GenTx(msgs, accnums, seq, priv...)
 	res := app.Check(tx)
 	return res
 }
 
 // simulate a block
-func SignCheckDeliver(t *testing.T, app *baseapp.BaseApp, msg sdk.Msg, accnums []int64, seq []int64, expPass bool, priv ...crypto.PrivKeyEd25519) {
+func SignCheckDeliver(t *testing.T, app *baseapp.BaseApp, msgs []sdk.Msg, accnums []int64, seq []int64, expPass bool, priv ...crypto.PrivKeyEd25519) sdk.Result {
 
 	// Sign the tx
-	tx := GenTx(msg, accnums, seq, priv...)
+	tx := GenTx(msgs, accnums, seq, priv...)
 
 	// Run a Check
 	res := app.Check(tx)
@@ -85,4 +106,5 @@ func SignCheckDeliver(t *testing.T, app *baseapp.BaseApp, msg sdk.Msg, accnums [
 	app.EndBlock(wrsp.RequestEndBlock{})
 
 	app.Commit()
+	return res
 }
