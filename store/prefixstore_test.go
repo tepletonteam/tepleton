9c7540a052be861103f79bd70c876453cc54768d
moon <mooncakeeos@yahoo.co.jp>
2020-04-15 16:35:33 +0800

    Merge branch develop into jlandrews/pruning

--- store/prefixstore_test.go
+++ store/prefixstore_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/tepleton/iavl"
-	dbm "github.com/tepleton/tmlibs/db"
+	dbm "github.com/tepleton/tepleton/libs/db"
 
 	sdk "github.com/tepleton/tepleton-sdk/types"
 )
@@ -66,7 +66,7 @@ func testPrefixStore(t *testing.T, baseStore KVStore, prefix []byte) {
 func TestIAVLStorePrefix(t *testing.T) {
 	db := dbm.NewMemDB()
 	tree := iavl.NewVersionedTree(db, cacheSize)
-	iavlStore := newIAVLStore(tree, numHistory)
+	iavlStore := newIAVLStore(tree, numRecent, storeEvery)
 
 	testPrefixStore(t, iavlStore, []byte("test"))
 }
