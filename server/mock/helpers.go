87fd7d351af8b9051e3aad8704c2f710dd9a15be
moon <mooncakeeos@yahoo.co.jp>
2020-04-21 16:48:57 +0800

    update dep for tm v0.22.0

--- server/mock/helpers.go
+++ server/mock/helpers.go
@@ -1,10 +1,11 @@
 package mock
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 
-	wrsp "github.com/tepleton/wrsp/types"
+	wrsp "github.com/tepleton/tepleton/wrsp/types"
 	"github.com/tepleton/tmlibs/log"
 )
 
@@ -19,7 +20,10 @@ func SetupApp() (wrsp.Application, func(), error) {
 	}
 
 	cleanup := func() {
-		os.RemoveAll(rootDir)
+		err := os.RemoveAll(rootDir)
+		if err != nil {
+			fmt.Printf("could not delete %s, had error %s\n", rootDir, err.Error())
+		}
 	}
 
 	app, err := NewApp(rootDir, logger)
