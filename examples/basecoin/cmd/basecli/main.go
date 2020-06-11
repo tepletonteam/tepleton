d1e124abda2a97627ad724b2dc82f98484288f67
moon <mooncakeeos@yahoo.co.jp>
2020-06-11 16:24:33 +0800

    Merge branch rigel/stake-spec-compliance

--- examples/basecoin/cmd/basecli/main.go
+++ examples/basecoin/cmd/basecli/main.go
@@ -3,24 +3,20 @@ package main
 import (
 	"os"
 
-	"github.com/spf13/cobra"
-
-	"github.com/tepleton/tmlibs/cli"
-
 	"github.com/tepleton/tepleton-sdk/client"
 	"github.com/tepleton/tepleton-sdk/client/keys"
 	"github.com/tepleton/tepleton-sdk/client/lcd"
 	"github.com/tepleton/tepleton-sdk/client/rpc"
 	"github.com/tepleton/tepleton-sdk/client/tx"
-
+	"github.com/tepleton/tepleton-sdk/examples/basecoin/app"
+	"github.com/tepleton/tepleton-sdk/examples/basecoin/types"
 	"github.com/tepleton/tepleton-sdk/version"
 	authcmd "github.com/tepleton/tepleton-sdk/x/auth/client/cli"
 	bankcmd "github.com/tepleton/tepleton-sdk/x/bank/client/cli"
 	ibccmd "github.com/tepleton/tepleton-sdk/x/ibc/client/cli"
 	stakecmd "github.com/tepleton/tepleton-sdk/x/stake/client/cli"
-
-	"github.com/tepleton/tepleton-sdk/examples/basecoin/app"
-	"github.com/tepleton/tepleton-sdk/examples/basecoin/types"
+	"github.com/spf13/cobra"
+	"github.com/tepleton/tepleton/libs/cli"
 )
 
 // rootCmd is the entry point for this binary
@@ -38,9 +34,9 @@ func main() {
 	// get the codec
 	cdc := app.MakeCodec()
 
-	// TODO: setup keybase, viper object, etc. to be passed into
+	// TODO: Setup keybase, viper object, etc. to be passed into
 	// the below functions and eliminate global vars, like we do
-	// with the cdc
+	// with the cdc.
 
 	// add standard rpc, and tx commands
 	rpc.AddCommands(rootCmd)
@@ -82,7 +78,7 @@ func main() {
 	executor := cli.PrepareMainCmd(rootCmd, "BC", os.ExpandEnv("$HOME/.basecli"))
 	err := executor.Execute()
 	if err != nil {
-		// handle with #870
+		// Note: Handle with #870
 		panic(err)
 	}
 }
