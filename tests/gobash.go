5a05bc72f512e4e8f83ed6a5e49feceb73ea70c9
moon <mooncakeeos@yahoo.co.jp>
2020-08-19 09:57:39 +0800

    Merge pull request #1509: Fix basecoin Example

--- tests/gobash.go
+++ tests/gobash.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	cmn "github.com/tepleton/tepleton/libs/common"
+	cmn "github.com/tepleton/tmlibs/common"
 )
 
 // Execute the command, return stdout, logging stdout/err to t.
